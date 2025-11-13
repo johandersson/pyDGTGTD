91f3881180801a22cbcf85bf313f14dda40c2918
Johan Andersson <johanbkandersson@gmail.com>
2025-11-13 14:47:28 +0100

    More

---
+++ check_folders.py
@@ -0,0 +1,9 @@
+from wxgtd.model.db import connect
+from wxgtd.model.objects import Folder
+
+Session = connect(r'C:\Users\Johan\.local\share\wxgtd\wxgtd.db')
+session = Session()
+folders = session.query(Folder).all()
+print(f'Folders in database: {len(folders)}')
+for f in folders:
+    print(f'  - UUID: {f.uuid} | Title: {f.title} | Visible: {f.visible}')
